863ce3ac5ca836b3f413207b4c2987dc200c9674
Desmond Roche <desmondroche@gmail.com>
2024-11-25 14:06:46 -0500

    Code added for this looping assignment

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import com.perscholas.com.loops.DisplaySum;
+import com.perscholas.com.loops.LoopDemoOne;
+import com.perscholas.com.loops.Palindrome;
+import com.perscholas.com.loops.Pyramid;
+
+public class Main {
+    public static void main(String[] args) {
+        //Calling classes with their own main methods
+        DisplaySum.main(args);
+        LoopDemoOne.main(args);
+        Palindrome.main(args);
+        Pyramid.main(args);
+    }
+}
